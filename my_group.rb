7094196f6a6e0a2745f09ff981c4d8d2c63048ce
clalil <cliljander@gmail.com>
2019-08-12 12:31:44 +0200

    Clean the code from unneccessary comments

--- my_group.rb
+++ my_group.rb
@@ -2,14 +2,10 @@
 person_1 = {name: "Anna", gender: "female", age: 20}
 person_2 = {name: "Bobo", gender: "male", age: 25}
 person_3 = {name: "Bella", gender: "female", age: 30}
-my_group = [person_1, person_2, person_3]
 
 my_group = [person_1, person_2, person_3]
-    my_group.each do |name, gender, age| 
-        puts "#{name} is a  #{gender} who is #{age} years old." 
+
+
+my_group.each do |item| 
+    puts "#{item[:name]} is a  #{item[:gender]} who is #{item[:age]} years old." 
 end
-
-# names = ['Anna', 'Bobo', 'Bella']
-#     names.each do |name|
-#   puts "Hello #{name}"
-# end
