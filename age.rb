137ad54aad5e907174458d8027cb39315b641b9e
clalil <cliljander@gmail.com>
2019-08-13 19:04:11 +0200

    add initial value to age

--- age.rb
+++ age.rb
@@ -5,7 +5,8 @@ def age
     name = gets.chomp
     puts "Hello, #{name}! Please enter your age:"
     age = gets.chomp.to_i
-    counting_years = 0
+    counting_years = 10
+    age += 10
     5.times do |user_age|
     puts "In #{counting_years} years you will be #{age} years old."
     age += 10
