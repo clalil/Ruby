3d4d3e04ab1dbad97491b7cb6abf061a0252d086
clalil <cliljander@gmail.com>
2019-08-13 14:23:24 +0200

    Add two_strings method

---
+++ thebasics.rb
@@ -0,0 +1,8 @@
+#The basics
+#Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+def two_strings
+    puts "Clarissa" + " " + "Liljander"
+end
+
+#Write a program that uses a hash to store a list of movie titles with the year they came out. 
+#Then use the puts command to make your program print out the year of each movie to the screen.
