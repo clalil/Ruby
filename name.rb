4e4d27377796a1ba263f281146b57da966309ff6
clalil <cliljander@gmail.com>
2019-08-13 14:59:11 +0200

    A program that asks the user for its name and returns a string in the shape of a greeting.

---
+++ name.rb
@@ -0,0 +1,7 @@
+#Write a program that asks the user to type in their name and then prints out a greeting message with their name included.
+
+def type_name
+    puts "Please enter your name:"
+    name = gets.chomp
+    puts = "Your name is: #{name}. What a beautiful name!"
+end
