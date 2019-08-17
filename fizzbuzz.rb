aabfc8e3b0444c5e6cb649872be1e0dbaaee98ed
clalil <cliljander@gmail.com>
2019-08-17 19:32:52 +0200

    Add two programs that can play the game FizzBuzz, one based on a numeric input and the other based on an array with already existing inputs as integers.

---
+++ fizzbuzz.rb
@@ -0,0 +1,37 @@
+#The objective is to write a program that takes a number or an array of numbers 
+#and returns “fizz”, “buzz”, “fizzbuzz” or the number (greater than zero) 
+#given certain conditions are fullfilled.
+#“fizz” if the number is divisible by 3
+#“buzz” if the number is divisible by 5
+#“fizzbuzz” if the number is divisible by 15
+#The same number if no other requirement is fulfilled.
+
+def fizzBuzz(num)
+        if num % 15 == 0
+            puts "FizBuzz"
+        elsif num % 5 == 0
+            puts "Buzz"
+        elsif num % 3 == 0
+            puts "Fizz"
+        else 
+            puts num
+        end
+end
+fizzBuzz(68)
+
+def fizzBuzz
+    array = [*1..100]
+    array.each { |x| puts x
+        if x % 15 == 0
+        puts "FizBuzz"
+        elsif x % 5 == 0
+        puts "Buzz"
+        elsif x % 3 == 0
+        puts "Fizz"
+        else 
+        puts x
+    end
+}
+end
+
+fizzBuzz
