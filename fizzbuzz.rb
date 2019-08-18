ebd6d17a8a26259f93299c45efa20bdaf0b20dad
clalil <cliljander@gmail.com>
2019-08-18 15:24:11 +0200

    Add function that takes an input and returns an error message if the input isn't a number.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,6 +6,8 @@
 #“fizzbuzz” if the number is divisible by 15
 #The same number if no other requirement is fulfilled.
 
+#Version 1: A program that takes number (no exceptions).
+
 def fizzBuzz(num)
         if num % 15 == 0
             puts "FizBuzz"
@@ -17,7 +19,31 @@ def fizzBuzz(num)
             puts num
         end
 end
-fizzBuzz(15)
+fizzBuzz(12)
+
+#Version 2: A program that takes a number and returns an error if the input
+#is a non-numeric value, a string value or an invalid integer.
+
+def fizzBuzz
+    puts "Enter a valid number between 0 and 100:"
+    num = gets.chomp.to_i
+    if num == 0 || num == 101
+        puts "That is not a valid number."
+    elsif
+        num % 15 == 0
+        puts "FizBuzz"
+    elsif num % 5 == 0
+        puts "Buzz"
+    elsif num % 3 == 0
+        puts "Fizz"
+    else 
+        puts num
+    end
+end
+
+fizzBuzz
+
+#Version 3: A program that takes an array of numbers.
 
 def fizzBuzz
     array = [*1..100]
