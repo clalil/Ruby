86bcc2cac1ac2a0f0d1c70263a430c42a0cd5f79
clalil <cliljander@gmail.com>
2019-08-29 08:39:34 +0200

    Add if/else statement and using uniq method to pass all tests for arrays

--- code_wars1.rb
+++ code_wars1.rb
@@ -1,5 +1,11 @@
 #Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 def unique_in_order(iterable)
+  if iterable.is_a? String
     iterable.squeeze().split('')
+    #Removes duplicates and separates letters from whole string
+  else iterable.kind_of? Array
+    iterable.uniq
+    #uniq works for arrays and as first method argument
   end
+end
