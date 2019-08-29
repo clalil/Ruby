4fbba2f034b603c675544bfb5f7c9c86781b6840
clalil <cliljander@gmail.com>
2019-08-29 08:48:00 +0200

    Refractor code from if/else to one-liner

--- code_wars1.rb
+++ code_wars1.rb
@@ -1,9 +1,5 @@
 #Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 
 def unique_in_order(iterable)
-  if iterable.is_a? String
-    iterable.squeeze().split('')
-  else iterable.kind_of? Array
-    iterable.uniq
-  end
+  iterable.is_a?(String) ? iterable.squeeze().split('') : iterable.uniq
 end
