0a6e3a2e7af0111a655dd337cc47cab3505732d0
clalil <cliljander@gmail.com>
2019-08-29 11:23:33 +0200

    Correct bug, pass final test, remove own commentary of CW kata 2

--- code_wars2.rb
+++ code_wars2.rb
@@ -1,9 +1,12 @@
+#Growth of a Population Kata: 
+#p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+#The function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
 def nb_year(p0, percent, aug, p)
     years = 0
     new_pop = p0
     while new_pop < p
-    #While new population < target population, each year increase new population with an additional percentage and flow of citizens
-      new_pop += (new_pop * percent / 100 + aug)
+      new_pop += (new_pop * percent / 100 + aug).to_i
       years += 1
     end
     return years
