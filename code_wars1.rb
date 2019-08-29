9cb3d3e09ba3f70213f95aa9ee60e1dcab674217
clalil <cliljander@gmail.com>
2019-08-29 08:40:10 +0200

    Remove personal notes from cw1 kata

--- code_wars1.rb
+++ code_wars1.rb
@@ -3,9 +3,7 @@
 def unique_in_order(iterable)
   if iterable.is_a? String
     iterable.squeeze().split('')
-    #Removes duplicates and separates letters from whole string
   else iterable.kind_of? Array
     iterable.uniq
-    #uniq works for arrays and as first method argument
   end
 end
