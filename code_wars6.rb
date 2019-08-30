b935d41b7835ca68cc83a0b7f18dfd9ff713f11a
clalil <cliljander@gmail.com>
2019-08-30 14:49:31 +0200

    Solve CW kata using built in Integer method

---
+++ code_wars6.rb
@@ -0,0 +1,7 @@
+#Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+def binary_array_to_number(arr)
+    integer = Integer((arr.join()), 2)
+    #convert array into string
+      #use built-in Integer method to convert string numbers into integer and binary
+  end 
